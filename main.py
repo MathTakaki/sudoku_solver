9339a16a64312f00ef436c892b923b020876f41a
Matheus Yuuji <yuuji061@usp.br>
2024-09-06 23:54:23 +0200

    ploting with matplotlib

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
-from src.solver import solver
-
-grid = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9],
-]
+from src.solver import init_plot, solver
 
 
-grid = solver(grid)
+def main():
+    grid = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9],
+    ]
 
-print(grid)
+    fig, ax = init_plot()
+    if solver(grid, ax, fig):
+        print("Sudoku solved:")
+        print(np.matrix(grid))
+    else:
+        print("No solution exists.")
+
+    plt.ioff()  # Turn off interactive mode
+    plt.show()  # Show the final plot
+
+
+if __name__ == "__main__":
+    main()
